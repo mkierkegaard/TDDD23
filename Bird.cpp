41b7f629cfe8194587beb20bae2e09df9a281c36
unknown <marki423@student.liu.se>
2014-09-15 19:03:15 +0200

    added Will

--- Bird.cpp
+++ Bird.cpp
@@ -7,10 +7,10 @@ Bird::Bird(cocos2d::Layer *layer){
 	visibleSize = Director::getInstance()->getVisibleSize();
 	origin = Director::getInstance()->getVisibleOrigin();
 
-	flappyBird = Sprite::create("ball.png");
+	flappyBird = Sprite::create("Will.png");
 	flappyBird->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
-	auto flappyBody = PhysicsBody::createCircle(flappyBird->getContentSize().width / 2);
+	auto flappyBody = PhysicsBody::createBox(Size(flappyBird->getContentSize().width, flappyBird->getContentSize().height) );
 	flappyBird->setPhysicsBody(flappyBody);
 
 	layer -> addChild(flappyBird, 100);
--- Pipe.cpp
+++ Pipe.cpp
@@ -11,43 +11,30 @@ Pipe::Pipe(){
 void Pipe::SpawnPipe(cocos2d::Layer *layer){
 	CCLOG("SPAWN PIPE");
 
-	auto topPipe = Sprite::create("Pipe.png");
-	auto buttomPipe = Sprite::create("Pipe.png");
+	auto topPipe = Sprite::create("ball.png");
 
 	auto topPipeBody = PhysicsBody::createBox(topPipe->getContentSize());
-	auto buttomPipeBody = PhysicsBody::createBox(buttomPipe->getContentSize());
 
 	auto random = CCRANDOM_0_1();
 
-	if (random < LOWER_SCREEN_PIPE_THRESHOLD){
-		random = LOWER_SCREEN_PIPE_THRESHOLD;
-	}
-	else if (random > UPPER_SCREEN_PIPE_THRESHOLD){
+	if (random > UPPER_SCREEN_PIPE_THRESHOLD){
 		random = UPPER_SCREEN_PIPE_THRESHOLD;
 	}
 
-	auto topPipePosition = (random*visibleSize.height + (topPipe->getContentSize().height / 2));
+	auto topPipePosition = (random*visibleSize.width + (topPipe->getContentSize().width / 2));
 
 
 	topPipeBody->setDynamic(false);
-	buttomPipeBody->setDynamic(false);
 
 	topPipeBody->setCollisionBitmask(OBSTACLE_COLLISION_MASK);
 	topPipeBody->setContactTestBitmask(true);
-	buttomPipeBody->setCollisionBitmask(OBSTACLE_COLLISION_MASK);
-	buttomPipeBody->setContactTestBitmask(true);
 	topPipe->setPhysicsBody(topPipeBody);
-	buttomPipe->setPhysicsBody(buttomPipeBody);
 
-	topPipe->setPosition(Point(visibleSize.width + topPipe->getContentSize().width + origin.x, topPipePosition));
-	buttomPipe->setPosition(Point(topPipe->getPositionX(), topPipePosition - (Sprite::create("ball.png")->getContentSize().height * PIPE_GAP) - topPipe->getContentSize().height));
+	topPipe->setPosition(Point(topPipePosition, -(visibleSize.height + topPipe->getContentSize().height + origin.x)));
 
 	layer->addChild(topPipe);
-	layer->addChild(buttomPipe);
 
-	auto topPipeAction = MoveBy::create(PIPE_MOVEMENT_SPEED * visibleSize.width, Point(-visibleSize.width * 1.5, 0));
-	auto buttomPipeAction = MoveBy::create(PIPE_MOVEMENT_SPEED * visibleSize.width, Point(-visibleSize.width * 1.5, 0));
+	auto topPipeAction = MoveBy::create(PIPE_MOVEMENT_SPEED * visibleSize.width, Point(0, visibleSize.height *3));
 
 	topPipe->runAction(topPipeAction);
-	buttomPipe->runAction(buttomPipeAction);
 }
